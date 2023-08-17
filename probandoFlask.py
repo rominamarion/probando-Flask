d40305677d41b90e6bf1438861c497c4e3bef020
Romina Ancaten <rominaancaten@hotmail.com>
2023-08-17 15:16:47 -0300

    actualizando flask

--- probandoFlask.py
+++ probandoFlask.py
@@ -1,8 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template #render_template, plantilla de Flask para ejecutar código HTML.
 
 app = Flask(__name__) #"app" es una variable para guardar el objeto que devuelve Flask (para iniciar Flask). Para crear urls, puede ser app o cualq otro nombre.
 
-@app.route("/") #Ruta para la pag principal
+@app.route("/") #Ruta para la pag principal, "/" solo.
 def index():
     return ("Hola Mundo") 
 
@@ -10,13 +10,14 @@ def index():
 def holaSpanish():
     return "Hola perro"
 
-@app.route("/hello")
-def hello():
-    return "Ohayo"
+@app.route("/nombre")
+def romina():
+    nombre = "Romina"
+    return render_template("nombre.html", nombre = nombre) #El primer "nombre" hace referencia a la variable que esta entre "{{}}" en la plantilla html. El segundo hace referencia a nombre = "Romina"
 
 @app.route("/about") #Ruta para about
 def about():
-    return render_template("about.html") #Devuelve la plantilla/template 
+    return render_template("about.html") #Dentro de una carpeta llamada "templates", va a buscar y a devolver la plantilla o template, en este caso "about"
 
 if __name__ == "__main__":
     app.run(debug=True) #Para no tener que reiniciar el servidor a cada rato, se pone debug=True(significa que es de prueba)
